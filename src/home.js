8702c0975313197d45f71e5c145b6349f33f158b
emmz3230 <emmz3230@gmail.com>
2022-02-07 10:46:03 -0800

    created an home component to show error and display the bloglist

--- src/home.js
+++ src/home.js
@@ -1,3 +1,21 @@
+import { useState } from "react";
 import blogList from "./bloglist";
 import usefetch  from "./usefetch";
 
+
+const home = () =>{
+    // use usefetch custom hook and pass url of the local json database
+    const {data:blog, isPending, error} = useState('http://localhost:8000/blogs');
+
+    // we then use the conditional display fromat to display incase we have an error
+    // and a Loading message and pass the blogs and the title to the blog list component which could display the blogs in a list
+return (
+    <div classname= "home"> 
+    {error && <div>{error}</div>}
+    {ispending && <div>{Loading}</div>}
+    <BlogList blogs = {blogs} title = "All Blogs"/>
+    </div>
+);
+}
+
+export default home;s
