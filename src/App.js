c5b5d1b38acb96b20f3ba2fe01553760603bdceb
emmz3230 <emmz3230@gmail.com>
2022-03-07 06:48:16 -0800

    make some changes and created a new contact file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./Home";
+import Contact from "./contact";
+import {browserRouter,Routes,route,Link, BrowserRouter} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <BrowserRouter>
+<Routes>
+  <Route exact path ="/" element ={<Home/>}></Route>
+  <Route exact path ="/" element ={<Contact/>}></Route>
+
+    </Routes>  
+</BrowserRouter>
+ return (
+
+   <div className="App">
+      <Link to= "/">Home</Link>
+      <Link to = "/contact">Contact</Link>
     </div>
+  
   );
 }
 
